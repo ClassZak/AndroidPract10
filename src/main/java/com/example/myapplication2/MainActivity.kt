b1968569a872cf005c7c14900829b984767aec53
Захар Голуб <pahargolub2@gmail.com>
2024-11-06 22:56:45 +0300

    FrameLayout

--- src/main/java/com/example/myapplication2/MainActivity.kt
+++ src/main/java/com/example/myapplication2/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.myapplication2
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.transition.Visibility
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,4 +23,29 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+
+    fun onApearButtonsClick(view: View):Unit{
+        var button:Button=view as Button
+        if (button != null)
+            when (view.id){
+                R.id.button1-> {
+                    DesappearButton(R.id.button1)
+                    AppearButton(R.id.button2)
+                }
+                R.id.button2->{
+                    DesappearButton(R.id.button2)
+                    AppearButton(R.id.button1)
+                }
+            }
+    }
+    fun AppearButton(buttonId: Int):Unit{
+        findViewById<Button>(buttonId).visibility= View.VISIBLE
+    }
+    fun DesappearButton(buttonId: Int):Unit{
+        findViewById<Button>(buttonId).visibility=View.INVISIBLE
+    }
 }
+
+
+
+
