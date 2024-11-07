b9ab487400a42ad9a019147e7b3559eaf76b9a1f
Захар Голуб <pahargolub2@gmail.com>
2024-11-07 03:07:30 +0300

    TableLayout is done

--- src/main/java/com/example/myapplication2/MainActivity.kt
+++ src/main/java/com/example/myapplication2/MainActivity.kt
@@ -18,13 +18,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.linerlayout)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.linerlayoutId)) { v, insets ->
+        setContentView(R.layout.tablelayout)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.tableLayout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        setTitle("LinearLayout")
+        setTitle("TableLayout")
     }
 
 
